4120c4410913d97d0d53cf005c55435277826601
Gary Soedarsono <garysoed@gmail.com>
2019-05-11 14:56:57 -0700

    Use WebpackBuilder

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,8 @@
 const glob = require("glob");
-const path = require("path");
-const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
-const WebpackNotifierPlugin = require('webpack-notifier');
 
-module.exports = {
-  entry: glob.sync("./src/**/*.test.ts"),
-  output: {
-    filename: "bundle.js",
-    path: __dirname + "/out"
-  },
-
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
-
-  resolve: {
-    alias: {
-      'tslib': path.resolve(__dirname, './node_modules/tslib'),
-    },
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json", ".html"],
-    plugins: [
-      new TsConfigPathsPlugin()
-    ]
-  },
-
-  module: {
-    rules: [
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      // { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader"},
-    ]
-  },
-
-  mode: "development",
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    "jasmine": "jasmine"
-  },
-
-  watch: true,
-
-  plugins: [
-    new WebpackNotifierPlugin({
-      alwaysNotify: true,
-      contentImage: '',
-      title: 'Thoth',
-    }),
-  ]
-};
+const WebpackBuilder = require('dev/webpack/builder');
+module.exports = (new WebpackBuilder(__dirname))
+    .addEntry('entry', glob.sync('./src/**/*.test.ts'))
+    .setOutput('bundle.js', '/out')
+    .addTypeScript()
+    .buildForDevelopment('Persona');
