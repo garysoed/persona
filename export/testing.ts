53a74e23457d2a0a6d7702c0e4075aa96b816258
Gary Soedarsono <garysoed@gmail.com>
2020-04-19 14:42:07 -0700

    Export type for PersonaTester

--- export/testing.ts
+++ export/testing.ts
@@ -2,5 +2,7 @@ export { BaseElementTester } from '../src/testing/base-element-tester';
 export { createFakeWindow } from '../src/testing/fake-window';
 export { ElementTester } from '../src/testing/element-tester';
 export { FakeCustomElementRegistry } from '../src/testing/fake-custom-element-registry';
-export { PersonaTester, PersonaTesterFactory } from '../src/testing/persona-tester';
+export { PersonaTesterFactory } from '../src/testing/persona-tester';
 export { PersonaTesterEnvironment } from '../src/testing/persona-tester-environment';
+
+export type { PersonaTester } from '../src/testing/persona-tester';
