1d976e76b6d71cf142b5e1ab72610c0008a7658a
Gary Soedarsono <garysoed@gmail.com>
2020-08-22 21:22:44 -0700

    Deleted render specs

--- src/render/render-spec.ts
+++
@@ -1,9 +0,0 @@
-import { Observable } from 'rxjs';
-
-export interface RenderSpec {
-  canReuseNode(node: Node): boolean;
-
-  createNode(): Observable<Node>;
-
-  registerNode(node: Node): Observable<unknown>;
-}
