c973d007782296ea42663143f62895f7c6aa48a4
Gary Soedarsono <garysoed@gmail.com>
2020-11-27 08:50:16 -0800

    Export BaseCtrlCtor

--- export/internal.ts
+++ export/internal.ts
@@ -2,6 +2,7 @@ export {CustomElementCtrlCtor} from '../src/types/custom-element-ctrl';
 export {Input} from '../src/types/input';
 export {Output} from '../src/types/output';
 export {ShadowRootLike} from '../src/types/shadow-root-like';
+export {BaseCtrlCtor} from '../src/core/base-ctrl';
 
 // Inputs and Outputs
 export {AttributeInput, UnresolvedAttributeInput} from '../src/input/attribute';
