052172310f14ea6a1386ba9ef64c8379397ad8be
Gary Soedarsono <garysoed@gmail.com>
2021-06-27 11:09:46 -0700

    Better typing for render.

--- src/render/render.ts
+++ src/render/render.ts
@@ -1,6 +1,7 @@
 import {Observable} from 'rxjs';
 
 import {ShadowContext} from '../core/shadow-context';
+import {UnresolvedSpec} from '../main/api';
 
 import {NodeWithId} from './node-with-id';
 import {renderCustomElement} from './render-custom-element';
@@ -9,14 +10,24 @@ import {renderElement} from './render-element';
 import {renderHtml} from './render-html';
 import {renderNode} from './render-node';
 import {renderTextNode} from './render-text-node';
+import {RenderCustomElementSpec} from './types/render-custom-element-spec';
+import {RenderElementSpec} from './types/render-element-spec';
+import {RenderFragmentSpec} from './types/render-fragment-spec';
+import {RenderHtmlSpec} from './types/render-html-spec';
+import {RenderNodeSpec} from './types/render-node-spec';
 import {RenderSpec} from './types/render-spec';
 import {RenderSpecType} from './types/render-spec-type';
+import {RenderTextNodeSpec} from './types/render-text-node-spec';
 
 
-export function render(
-    spec: RenderSpec,
-    context: ShadowContext,
-): Observable<NodeWithId<Node>|null> {
+export function render(spec: RenderCustomElementSpec<UnresolvedSpec>, context: ShadowContext): Observable<NodeWithId<Element>>;
+export function render(spec: RenderElementSpec, context: ShadowContext): Observable<NodeWithId<Element>>;
+export function render(spec: RenderFragmentSpec, context: ShadowContext): Observable<NodeWithId<DocumentFragment>>;
+export function render(spec: RenderHtmlSpec, context: ShadowContext): Observable<NodeWithId<Element>>;
+export function render(spec: RenderNodeSpec<Node>, context: ShadowContext): Observable<NodeWithId<Node>>;
+export function render(spec: RenderTextNodeSpec, context: ShadowContext): Observable<NodeWithId<Text>>;
+export function render(spec: RenderSpec, context: ShadowContext): Observable<NodeWithId<Node>|null>;
+export function render(spec: RenderSpec, context: ShadowContext): Observable<NodeWithId<Node>|null> {
   switch (spec.type) {
     case RenderSpecType.CUSTOM_ELEMENT:
       return renderCustomElement(spec, context);
