ab616ba912b5b2ff2f637ba0e302e09961ce21dc
Gary Soedarsono <garysoed@gmail.com>
2021-11-24 15:20:18 -0800

    Extracted reverseSpec to external file

--- src-next/selector/id.ts
+++ src-next/selector/id.ts
@@ -1,43 +1,12 @@
-import {iattr} from '../input/attr';
-import {iclass} from '../input/class';
-import {ievent} from '../input/event';
-import {iflag} from '../input/flag';
-import {ivalue} from '../input/value';
-import {oattr} from '../output/attr';
-import {oclass} from '../output/class';
-import {oevent} from '../output/event';
-import {oflag} from '../output/flag';
-import {ovalue} from '../output/value';
 import {RenderContext} from '../render/types/render-context';
-import {ResolvedBindingSpecProvider, ResolvedProvider, Spec, UnresolvedBindingSpec, UnresolvedIO} from '../types/ctrl';
-import {ApiType, IAttr, IClass, IEvent, IFlag, InputOutput, IOType, IValue, OAttr, OClass, OEvent, OFlag, OSingle, OValue} from '../types/io';
+import {ResolvedBindingSpecProvider, ResolvedProvider, Spec, UnresolvedIO} from '../types/ctrl';
+import {IAttr, IClass, IEvent, IFlag, InputOutput, OAttr, OClass, OFlag, OSingle} from '../types/io';
 import {Registration} from '../types/registration';
+import {ReversedSpec, reverseSpec} from '../util/reverse-spec';
 
-type ReversedIO<T> =
-    T extends IAttr ? OAttr :
-    T extends OAttr ? IAttr :
-    T extends IClass ? OClass :
-    T extends OClass ? IClass :
-    T extends OEvent ? IEvent :
-    T extends IFlag ? OFlag :
-    T extends OFlag ? IFlag :
-    T extends IValue<infer V> ? OValue<V> :
-    T extends OValue<infer V> ? IValue<V> :
-    never;
-
-type ReversedSpec<U extends UnresolvedBindingSpec> = UnresolvedBindingSpec & {
-  readonly [K in keyof U]: ReversedIO<U[K]>;
-};
 
 type RegistrationWithSpec<S extends Spec> = Pick<Registration<HTMLElement, S>, 'spec'>;
 
-type ReversableIO =
-    IAttr|OAttr|
-    IClass|OClass|
-    OEvent|IEvent|
-    IFlag|OFlag|
-    OSingle|
-    IValue<any>|OValue<any>;
 
 function getElement(root: ShadowRoot, id: string): HTMLElement {
   const el = root.getElementById(id);
@@ -45,62 +14,6 @@ function getElement(root: ShadowRoot, id: string): HTMLElement {
     throw new Error(`Element with ID ${id} cannot be found`);
   }
   return el;
-}
-
-function reverse<U extends UnresolvedBindingSpec>(spec: U): ReversedSpec<U> {
-  const reversed: Partial<Record<keyof U, ReversableIO>> = {};
-
-  for (const key in spec) {
-    reversed[key] = reverseIO(spec[key]);
-  }
-  return reversed as ReversedSpec<U>;
-}
-
-function reverseIO<T extends ReversableIO>(io: T): ReversedIO<T>;
-function reverseIO(io: ReversableIO): InputOutput {
-  switch (io.apiType) {
-    case ApiType.ATTR:
-      switch (io.ioType) {
-        case IOType.INPUT:
-          return oattr(io.attrName);
-        case IOType.OUTPUT:
-          return iattr(io.attrName);
-      }
-      break;
-    case ApiType.CLASS:
-      switch (io.ioType) {
-        case IOType.INPUT:
-          return oclass(io.className);
-        case IOType.OUTPUT:
-          return iclass(io.className);
-      }
-      break;
-    case ApiType.EVENT:
-      switch (io.ioType) {
-        case IOType.INPUT:
-          return oevent(io.eventName);
-        case IOType.OUTPUT:
-          return ievent(io.eventName);
-      }
-      break;
-    case ApiType.FLAG:
-      switch (io.ioType) {
-        case IOType.INPUT:
-          return oflag(io.attrName);
-        case IOType.OUTPUT:
-          return iflag(io.attrName);
-      }
-      break;
-    case ApiType.SINGLE:
-      throw new Error(`Unsupported reversal for ${io.apiType}`);
-    case ApiType.VALUE:
-      switch (io.ioType) {
-        case IOType.INPUT:
-          return ovalue(io.key, io.valueType, io.defaultValue);
-        case IOType.OUTPUT:
-          return ivalue(io.key, io.valueType, io.defaultValue);
-      }
-  }
 }
 
 
@@ -128,7 +41,7 @@ export function id<S extends Spec, X extends ExtraUnresolvedBindingSpec>(
     extra?: X,
 ): ResolvedBindingSpecProvider<ReversedSpec<S['host']&{}> & X> {
   const providers: Partial<Record<string, ResolvedProvider<InputOutput>>> = {};
-  const reversed = reverse(registration.spec.host ?? {});
+  const reversed = reverseSpec(registration.spec.host ?? {});
   for (const key in reversed) {
     providers[key] = (root: ShadowRoot, context: RenderContext) => reversed[key].resolve(
         getElement(root, id),
---
+++ src-next/util/reverse-spec.ts
@@ -0,0 +1,93 @@
+import {iattr} from '../input/attr';
+import {iclass} from '../input/class';
+import {ievent} from '../input/event';
+import {iflag} from '../input/flag';
+import {ivalue} from '../input/value';
+import {oattr} from '../output/attr';
+import {oclass} from '../output/class';
+import {oevent} from '../output/event';
+import {oflag} from '../output/flag';
+import {ovalue} from '../output/value';
+import {UnresolvedBindingSpec} from '../types/ctrl';
+import {ApiType, IAttr, IClass, IEvent, IFlag, InputOutput, IOType, IValue, OAttr, OClass, OEvent, OFlag, OSingle, OValue} from '../types/io';
+
+
+type ReversedIO<T> =
+    T extends IAttr ? OAttr :
+    T extends OAttr ? IAttr :
+    T extends IClass ? OClass :
+    T extends OClass ? IClass :
+    T extends OEvent ? IEvent :
+    T extends IFlag ? OFlag :
+    T extends OFlag ? IFlag :
+    T extends IValue<infer V> ? OValue<V> :
+    T extends OValue<infer V> ? IValue<V> :
+    never;
+
+export type ReversedSpec<U extends UnresolvedBindingSpec> = UnresolvedBindingSpec & {
+  readonly [K in keyof U]: ReversedIO<U[K]>;
+};
+
+type ReversableIO =
+    IAttr|OAttr|
+    IClass|OClass|
+    OEvent|IEvent|
+    IFlag|OFlag|
+    OSingle|
+    IValue<any>|OValue<any>;
+
+export function reverseSpec<U extends UnresolvedBindingSpec>(spec: U): ReversedSpec<U> {
+  const reversed: Partial<Record<keyof U, ReversableIO>> = {};
+
+  for (const key in spec) {
+    reversed[key] = reverseIO(spec[key]);
+  }
+  return reversed as ReversedSpec<U>;
+}
+
+function reverseIO<T extends ReversableIO>(io: T): ReversedIO<T>;
+function reverseIO(io: ReversableIO): InputOutput {
+  switch (io.apiType) {
+    case ApiType.ATTR:
+      switch (io.ioType) {
+        case IOType.INPUT:
+          return oattr(io.attrName);
+        case IOType.OUTPUT:
+          return iattr(io.attrName);
+      }
+      break;
+    case ApiType.CLASS:
+      switch (io.ioType) {
+        case IOType.INPUT:
+          return oclass(io.className);
+        case IOType.OUTPUT:
+          return iclass(io.className);
+      }
+      break;
+    case ApiType.EVENT:
+      switch (io.ioType) {
+        case IOType.INPUT:
+          return oevent(io.eventName);
+        case IOType.OUTPUT:
+          return ievent(io.eventName);
+      }
+      break;
+    case ApiType.FLAG:
+      switch (io.ioType) {
+        case IOType.INPUT:
+          return oflag(io.attrName);
+        case IOType.OUTPUT:
+          return iflag(io.attrName);
+      }
+      break;
+    case ApiType.SINGLE:
+      throw new Error(`Unsupported reversal for ${io.apiType}`);
+    case ApiType.VALUE:
+      switch (io.ioType) {
+        case IOType.INPUT:
+          return ovalue(io.key, io.valueType, io.defaultValue);
+        case IOType.OUTPUT:
+          return ivalue(io.key, io.valueType, io.defaultValue);
+      }
+  }
+}
