a09c722ffeb10ae2ed73a8fe179450d1b216e12f
Gary Soedarsono <garysoed@gmail.com>
2021-11-27 11:41:16 -0800

    Improved simulate mouse events

--- src-next/testing/get-el.ts
+++ src-next/testing/get-el.ts
@@ -5,9 +5,27 @@ interface Options {
   readonly shiftKey?: boolean;
 }
 
+interface ClickEvents {
+  readonly down: MouseEvent;
+  readonly up: MouseEvent;
+  readonly click: MouseEvent;
+}
+
+interface MouseOutEvents {
+  readonly leaves: readonly MouseEvent[];
+  readonly out: MouseEvent;
+}
+
+interface MouseOverEvents {
+  readonly enters: readonly MouseEvent[];
+  readonly over: MouseEvent;
+}
+
 type Harness = HTMLElement & {
-  simulateClick(): MouseEvent;
+  simulateClick(): ClickEvents;
   simulateKeydown(key: string, options?: Options): KeyboardEvent;
+  simulateMouseOut(): MouseOutEvents;
+  simulateMouseOver(): MouseOverEvents;
 };
 
 export function getEl(el: HTMLElement, id: string): Harness|null {
@@ -17,10 +35,46 @@ export function getEl(el: HTMLElement, id: string): Harness|null {
   }
 
   return Object.assign(element, {
-    simulateClick(): MouseEvent {
-      const event = new MouseEvent('click');
-      element.dispatchEvent(event);
-      return event;
+    simulateClick(): ClickEvents {
+      const down = new MouseEvent('mousedown');
+      element.dispatchEvent(down);
+      const up = new MouseEvent('mouseup');
+      element.dispatchEvent(up);
+      const click = new MouseEvent('click');
+      element.dispatchEvent(click);
+      return {down, up, click};
+    },
+
+    simulateMouseOut(): MouseOutEvents {
+      const out = new MouseEvent('mouseout');
+      element.dispatchEvent(out);
+
+      const leaves: MouseEvent[] = [];
+      let curr: HTMLElement|null = element;
+      while(curr !== null) {
+        const enter = new MouseEvent('mouseleave');
+        curr.dispatchEvent(enter);
+        leaves.push(enter);
+        curr = curr.parentElement;
+      }
+
+      return {leaves, out};
+    },
+
+    simulateMouseOver(): MouseOverEvents {
+      const over = new MouseEvent('mouseover');
+      element.dispatchEvent(over);
+
+      const enters: MouseEvent[] = [];
+      let curr: HTMLElement|null = element;
+      while(curr !== null) {
+        const enter = new MouseEvent('mouseenter');
+        curr.dispatchEvent(enter);
+        enters.push(enter);
+        curr = curr.parentElement;
+      }
+
+      return {enters, over};
     },
 
     simulateKeydown(key: string, options: Options = {}): KeyboardEvent {
